03a9d4247b8d853f3ff2790f3a5c3bb7226c8c79
conrad <yhg0337@naver.com>
2025-03-11 22:14:03 +0900

    Add tailwind, convention, and project structure configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,47 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import {FlatCompat} from "@eslint/eslintrc";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    //수정하기
+    env: {
+      browser: true,
+      node: true,
+    },
+    extends: ["eslint:recommended", "next"],
+    plugins: ["prettier", "@typescript-eslint"],
+    rules: {
+      // "@next/next/no-img-element": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: true,
+          semi: true,
+          useTabs: false,
+          tabWidth: 2,
+          printWidth: 102,
+          bracketSpacing: true,
+          arrowParens: "avoid",
+          endOfLine: "auto",
+        },
+      ],
+    },
+    parserOptions: {
+      parser: "@typescript-eslint/parser",
+    },
+  }),
+  // ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
 ];
 
 export default eslintConfig;
