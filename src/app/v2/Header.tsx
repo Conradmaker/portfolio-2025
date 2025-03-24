919de9651847d19824b4112b1fe42fd113df65d5
conrad <yhg0337@naver.com>
2025-03-24 22:49:52 +0900

    Update header and navigation component

--- src/app/v2/Header.tsx
+++ src/app/v2/Header.tsx
@@ -1,36 +1,167 @@
 'use client';
-import { Button } from '@/components/ui/button';
 import Svg from '@/components/ui/svg';
-import { useUiStore } from '@/modules/zustand/ui';
-import { useScroll, useTransform, motion } from 'motion/react';
-import React from 'react';
+import { useScroll, useTransform, motion, AnimatePresence, Variants } from 'motion/react';
+import React, { useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { ChevronDown, LucideBookOpenText, LucideGithub, LucideMail } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 export default function Header() {
-  const { scrollY } = useScroll({});
+  const { scrollY } = useScroll();
+  const containerRef = useRef<HTMLDivElement>(null);
   const headerBorder = useTransform(scrollY, [750, 800], [0, 1]);
-  const { navOpen, setNavOpen } = useUiStore();
+  const [contactOpen, setContactOpen] = useState(false);
+  const [currentTab, setCurrentTab] = useState<'about' | 'skills' | 'works'>('about');
+
+  const variants: Variants = {
+    animate: (custom: number) => ({
+      opacity: 1,
+      x: 0,
+      transition: { delay: custom * 0.1 },
+    }),
+    exit: (custom: number) => ({
+      opacity: 0,
+      x: 100,
+      transition: { delay: custom * 0.1 },
+    }),
+    initial: (custom: number) => ({
+      opacity: 0,
+      x: 100,
+      transition: { delay: custom * 0.1 },
+    }),
+  };
+
+  useEffect(() => {
+    scrollY.on('change', value => {
+      if (value < 6600) {
+        setCurrentTab('about');
+      } else if (value < 11700) {
+        setCurrentTab('skills');
+      } else {
+        setCurrentTab('works');
+      }
+    });
+    return () => {
+      scrollY.clearListeners();
+    };
+  }, []);
+  useEffect(() => {
+    setContactOpen(false);
+  }, [currentTab]);
+
   return (
-    <motion.header className="w-full sticky top-0 z-30 bg-white/30 backdrop-blur-sm antialiased">
+    <motion.header
+      ref={containerRef}
+      className="w-full bg-white/30 backdrop-blur-sm  sticky top-0 z-30 antialiased"
+    >
       <div className="flex items-center h-[72px] relative max-w-[1600px] mx-auto w-full justify-between">
         <div className="group/logo text-3xl relative flex items-center cursor-pointer">
           <Svg.Logo className="bg-slate-200/0 w-44" />
           <div className="group-hover/logo:opacity-100 opacity-0 size-7 ml-1 rounded-full bg-blue-700 transition-all duration-300 absolute cursor-pointer"></div>
         </div>
-        <div className="text-3xl relative flex gap-2">
-          <div className="transition-all cursor-pointer text-sm hover:border border-gray-100 hover:bg-white/60 hover:text-black bg-white/30  text-black/50 w-28 h-10 rounded-xl flex items-center justify-center">
-            SCROLL UP
-          </div>
-          <div className="transition-all cursor-pointer hover:bg-black hover:text-white text-sm bg-black/40 w-28 h-10 rounded-xl text-white flex items-center justify-center">
-            MENU
-          </div>
-          {/* <p
-            onClick={() => setNavOpen(!navOpen)}
-            className="group-hover/menu:text-white w-20 text-blue-700 transition-all duration-300 relative z-10 cursor-pointer "
+        <ul className="flex font-medium">
+          <Button
+            variant={currentTab === 'about' ? 'default' : 'ghost'}
+            className={'rounded-full cursor-pointer'}
+            onClick={() => {
+              window.scrollTo({ top: 0, behavior: 'smooth' });
+            }}
           >
-            Menu
-          </p>
-          <div className="group-hover/menu:w-22 w-0 h-9 top-0 right-0 bg-blue-700 transition-all duration-300 absolute cursor-pointer"></div> */}
-        </div>
+            About
+          </Button>
+          <Button
+            variant={currentTab === 'skills' ? 'default' : 'ghost'}
+            className={'rounded-full'}
+            onClick={() => {
+              window.scrollTo({ top: 6600, behavior: 'smooth' });
+            }}
+          >
+            Skills
+          </Button>
+          <Button
+            variant={currentTab === 'works' ? 'default' : 'ghost'}
+            className={'rounded-full'}
+            onClick={() => {
+              window.scrollTo({ top: 12700, behavior: 'smooth' });
+            }}
+          >
+            Works
+          </Button>
+          <Button
+            variant="ghost"
+            className={cn(
+              contactOpen ? 'bg-black text-white hover:bg-black/60 hover:text-white' : '',
+              'rounded-full cursor-pointer gap-0.5',
+            )}
+            onClick={() => setContactOpen(!contactOpen)}
+          >
+            <motion.p className="inline-block w-13">Contact </motion.p>
+            <ChevronDown
+              className={cn('transition-all duration-500', contactOpen ? '-scale-y-100' : '')}
+            />
+          </Button>
+          <AnimatePresence>
+            {contactOpen && (
+              <ul
+                className={cn('flex menu-contact flex-col items-end absolute right-0 top-19 gap-2')}
+              >
+                <motion.li
+                  variants={variants}
+                  custom={1}
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                >
+                  <Link href="mailto:yhg0337@gmail.com">
+                    <p className="transition-all duration-300 group/link self-end flex items-center hover:bg-[#001713] bg-gray-100 p-1 rounded-full text-sm gap-1 pl-3 cursor-pointer">
+                      <span className="group-hover/link:text-white transition-all duration-300">
+                        yhg0337@gmail.com
+                      </span>
+                      <LucideMail className="size-6 p-1 bg-white rounded-full" />
+                    </p>
+                  </Link>
+                </motion.li>
+                <motion.li
+                  variants={variants}
+                  custom={2}
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                >
+                  <Link
+                    href="https://github.com/Conradmaker"
+                    target="_blank"
+                    className="transition-all duration-300 group/link self-end flex items-center hover:bg-[#001713] bg-gray-100 p-1 rounded-full text-sm gap-1 pl-3 cursor-pointer"
+                  >
+                    <span className="group-hover/link:text-white transition-all duration-300">
+                      @conradmaker
+                    </span>
+                    <LucideGithub className="size-6 p-1 bg-white rounded-full" />
+                  </Link>
+                </motion.li>
+                <motion.li
+                  variants={variants}
+                  custom={3}
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                >
+                  <Link
+                    href="https://velog.io/@yhg0337/posts"
+                    target="_blank"
+                    className="transition-all duration-300 group/link self-end flex items-center hover:bg-[#001713] bg-gray-100 p-1 rounded-full text-sm gap-1 pl-3 cursor-pointer"
+                  >
+                    <span className="group-hover/link:text-white transition-all duration-300">
+                      @Conrad.log
+                    </span>
+                    <LucideBookOpenText className="size-6 p-1 bg-white rounded-full" />
+                  </Link>
+                </motion.li>
+              </ul>
+            )}
+          </AnimatePresence>
+        </ul>
       </div>
 
       <motion.div className="h-px bg-slate-200 w-full" style={{ opacity: headerBorder }}></motion.div>
