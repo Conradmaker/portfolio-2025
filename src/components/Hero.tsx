e701cb585d9b9f687d8ab48e733cbd7b1ee6fd92
conrad <yhg0337@naver.com>
2025-04-03 23:32:40 +0900

    Update Hero, Process, and Skills components for improved responsiveness and layout
    - 섹션 반응형 레이아웃 업데이트

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,24 +3,28 @@ import Svg from '@/components/ui/svg';
 import { useScroll, useTransform, motion } from 'motion/react';
 import React, { useRef } from 'react';
 import { FlipWords } from './ui/filp-words';
+import { AspectRatio } from './ui/aspect-ratio';
+import { useUiStore } from '@/modules/zustand/ui';
 
 export default function Hero() {
   const containerRef = useRef<HTMLDivElement>(null);
+  const { isMobile } = useUiStore();
   const { scrollY } = useScroll({
     target: containerRef,
   });
   const heroScale = useTransform(scrollY, [0, 500, 700], [1, 0.9, 0.85]);
+  const heroScaleMobile = useTransform(scrollY, [0, 500, 700], [1, 0.75, 0.6]);
   const heroY = useTransform(scrollY, [0, 800], [0, 800]);
   return (
     <>
-      <section id="about" className="max-w-[1600px] mx-auto" ref={containerRef}>
+      <section id="about" className="max-w-[1632px] px-3 md:px-4 mx-auto" ref={containerRef}>
         <motion.div
-          style={{ scale: heroScale, y: heroY }}
-          className="h-[760px] w-full bg-[#F7F8FA] rounded-2xl relative"
+          style={{ scale: isMobile ? heroScaleMobile : heroScale, y: heroY }}
+          className="mb-4 max-h-[760px] w-full bg-[#F7F8FA] rounded-2xl relative transition-all duration-[0.025s]"
         >
-          <div className="flex items-center h-full pl-14">
-            <div className="space-y-24 flex-1">
-              <h1 className="text-8xl font-medium leading-tight">
+          <div className="flex flex-col lg:flex-row lg:items-center justify-center h-full lg:pl-14">
+            <div className="px-6 py-9 lg:px-0 lg:py-0 space-y-4 lg:space-y-20 lg:flex-1">
+              <h1 className="text-[8vw] sm:text-6xl md:text-7xl lg:text-5xl xl:text-8xl font-semibold sm:font-medium leading-tight">
                 안녕하세요.
                 <br />
                 <FlipWords
@@ -33,35 +37,48 @@ export default function Hero() {
                     '디자인에 민감한',
                   ]}
                   duration={1200}
-                  className="text-blue-800"
+                  className="text-blue-800 -translate-x-2.5 md:-translate-x-0"
                 />
                 <br />
                 개발자 유원근입니다.
               </h1>
-              <p className="text-foreground/60">
-                저는 광범위한 디지털 제품 전문 지식과 고객의 비즈니스 목표를 결합합니다.
+              <p className="pb-5 lg:pb-0 lg:pl-2 text-sm lg:text-xl text-foreground/60 leading-[1.75]">
+                저는 풀스택 개발자로서 <br />
+                프론트엔드, 백엔드를 아울러 창의적인 문제 해결과 <br />
+                고객과 팀의 요구를 충족시키는 데 최선을 다할 것입니다.
               </p>
             </div>
-
-            <ul className="h-full grid grid-cols-2 grid-rows-3">
-              <li className="h-full translate-x-9">
-                <Svg.Hero3 className="h-full" />
-              </li>
-              <li className="h-full">
-                <Svg.Hero4 className="h-full translate-x-3" color="#000" />
-              </li>
-              <li className="h-full">
-                <Svg.Hero5 className="h-full translate-x-9" color="#000" />
-              </li>
-              <li className="h-full">
-                <Svg.Hero2 className="h-full translate-x-3" />
-              </li>
-              <li className="h-full">
-                <Svg.Hero1 className="h-full translate-x-9" />
-              </li>
-              <li className="h-full">
-                <Svg.Hero6 className="h-full translate-x-3" color="#000" />
-              </li>
+            <ul className="grid h-full grid-cols-3 grid-rows-2 lg:grid-cols-2 lg:grid-rows-3 w-full lg:max-w-[504px]">
+              <AspectRatio ratio={1 / 1} className="">
+                <li className="h-full">
+                  <Svg.Hero3 className="max-w-full max-h-full" />
+                </li>
+              </AspectRatio>
+              <AspectRatio ratio={1 / 1} className="">
+                <li className="h-full">
+                  <Svg.Hero4 className="max-w-full max-h-full" color="#000" />
+                </li>
+              </AspectRatio>
+              <AspectRatio ratio={1 / 1} className="">
+                <li className="h-full">
+                  <Svg.Hero5 className="max-w-full max-h-full" color="#000" />
+                </li>
+              </AspectRatio>
+              <AspectRatio ratio={1 / 1} className="order-3">
+                <li className="h-full">
+                  <Svg.Hero2 className="max-w-full max-h-full" />
+                </li>
+              </AspectRatio>
+              <AspectRatio ratio={1 / 1} className="order-2">
+                <li className="h-full">
+                  <Svg.Hero1 className="max-w-full max-h-full" />
+                </li>
+              </AspectRatio>
+              <AspectRatio ratio={1 / 1} className="">
+                <li className="h-full">
+                  <Svg.Hero6 className="max-w-full max-h-full" color="#000" />
+                </li>
+              </AspectRatio>
             </ul>
           </div>
         </motion.div>
--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -3,22 +3,31 @@ import React from 'react';
 
 export default function Process() {
   return (
-    <section id="process" className="second my-44 z-0 relative max-w-[1600px] mx-auto break-keep">
-      <div className="flex items-end justify-between">
-        <h2 className="text-black text-6xl font-medium leading-tight">
-          프로젝트의 <span>ZERO</span>에서 <br /> <span>HERO</span> 그 이상까지 함께할 수 있는 개발자
+    <section
+      id="process"
+      className="second my-44 z-0 relative max-w-[1632px] mx-auto break-keep px-4"
+    >
+      <div className="flex flex-col xl:flex-row xl:items-end justify-between">
+        <h2 className="mb-4 xl:mb-0 text-black text-[7.4vw] md:text-5xl xl:text-6xl font-semibold md:font-medium leading-snug md:leading-tight">
+          프로젝트의 <span>ZERO</span>에서 <br /> <span>HERO</span> 그 이상까지{' '}
+          <br className="md:hidden" /> 함께할 수 있습니다.
         </h2>
-        <p className="w-[480px] text-2xl leading-relaxed">
-          다양한 분야의 경험과 지식을 융합하고 이용하여, 다른 팀원이 발견할 수 없는 부분까지 넓고 멀리
-          생각하기 위해 노력합니다.
+        <p className="lg:w-[480px] text-[15px] lg:text-lg xl:text-2xl leading-relaxed break-keep">
+          다양한 분야의 경험과 지식을 융합하고 이용하여,
+          <br className="md:hidden" /> 팀원들이 발견할 수 없는 부분까지 넓고 멀리 생각하기 위해
+          노력합니다.
         </p>
       </div>
-      <ul className="grid grid-cols-3 mt-24 gap-x-16 gap-y-20">
+      <ul className="grid md:grid-cols-2 xl:grid-cols-3 mt-24 gap-x-16 gap-y-14 lg:gap-y-20">
         {sectors.map((sector, idx) => (
           <li key={idx} className="w-full">
             {sector.svg}
-            <h3 className="text-3xl mb-4 mt-5 font-semibold">{sector.sector}</h3>
-            <p className="text-base leading-[1.75] text-foreground/70 break-keep">{sector.desc}</p>
+            <h3 className="text-2xl sm:text-3xl mb-3 sm:mb-4 mt-4 sm:mt-5 font-semibold">
+              {sector.sector}
+            </h3>
+            <p className="text-[15px] md:text-base leading-[1.75] text-foreground/70 break-keep">
+              {sector.desc}
+            </p>
           </li>
         ))}
       </ul>
@@ -29,32 +38,32 @@ export default function Process() {
 const sectors = [
   {
     sector: '기획',
-    svg: <Svg.Hero1 className="size-24" />,
-    desc: '데이터에 기반한 유저의 데이터와 다양한 프로덕트에대한 사용경험을 최대한 살려 와우모먼트를 이끌어내거나 도울 수 있습니다.',
+    svg: <Svg.Hero1 className="size-16 md:size-24" />,
+    desc: '데이터에 기반한 유저의 데이터와 다양한 프로덕트에 대한 사용경험을 최대한 살려 와우모먼트를 이끌어내거나 도울 수 있습니다.',
   },
   {
     sector: '리서치',
-    svg: <Svg.Hero5 className="size-24" />,
+    svg: <Svg.Hero5 className="size-16 md:size-24" />,
     desc: '정성적, 정량적 리서치에 대한 설계가 가능하며, 의사결정에 필요한 데이터를 얻기위한 Mixpanel, Amplitude, Firefly등의 툴을 활용 뿐만 아닌, 데이터 텍소노미를 효과적으로 설계 구축해 프로젝트의 방향성을 명확히 제시합니다.',
   },
   {
     sector: 'UI / UX 디자인',
-    svg: <Svg.Hero3 className="size-24" />,
+    svg: <Svg.Hero3 className="size-16 md:size-24" />,
     desc: '디자인 시스템을 디자이너와 함께 양자의 관점에서 설계 및 적용할 수 있고, 효율적 의사소통을 위해 UX, UI등의 이론에 대하여 공부하고 소통할 수 있으며, 기본적인 툴들 또한 활용할 수 있습니다.',
   },
   {
     sector: '설계',
-    svg: <Svg.Hero4 className="size-24" />,
+    svg: <Svg.Hero4 className="size-16 md:size-24" />,
     desc: '3대(Azure, AWS, Google)+국내(NCP,NHN...) 등 다양한 클라우드에 대한 깊은 사용경험과 폭넓은 SaaS 정보를 통해 틀에 박힌 설계가 아닌 비용과 성능에 최적화된 아키텍쳐를 설계할 수 있습니다.',
   },
   {
     sector: '개발',
-    svg: <Svg.Hero2 className="size-24" />,
+    svg: <Svg.Hero2 className="size-16 md:size-24" />,
     desc: '코드의 안정성을 바탕으로 한 빠른 개발이 가능하며, 프론트와 백엔드를 가리지 않는 다양한 언어와 프레임워크에 대한 경험을 바탕으로 다양한 요구조건에 대한 유연한 대처와 효율적인 개발이 가능합니다. 또한 개발에 대한 긍정적 에너지를 팀원에게 전파하고자 노력합니다.',
   },
   {
     sector: '유지보수',
-    svg: <Svg.Hero6 className="size-24" />,
+    svg: <Svg.Hero6 className="size-16 md:size-24" />,
     desc: '다양한 마이그레이션, 업데이트의 경험을 바탕으로, 기존 시스템에 대한 안정성을 확보하고 기술&비용적 최적화를 할 수 있으며, 모니터링 시스템과 자동화된 CI/CD를 구축하여 배포 및 운영환경을 개선할 수 있습니다.',
   },
 ];
